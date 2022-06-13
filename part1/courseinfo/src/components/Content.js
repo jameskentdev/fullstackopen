69a44f1ec5f7034fbe02cbf30ac550a19afc9684
captainkent <29131214+captainkent@users.noreply.github.com>
2022-06-13 21:08:23 +0200

    Updated exercise 1.1.

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Content = (props) => {
+
+    let joinedArray = [];
+    for (let i = 0; i < props.exercises.length; i++) {
+        joinedArray.push({exercises: props.exercises[i], part: props.parts[i]});
+    }
+
     return (
         <div>
-            <p>
-                {props.part} {props.exercises}
-            </p>
+            { joinedArray.map(function ({exercises, part}, index) {
+                return <p key={index}>{part} {exercises} </p>
+            })}
         </div>
     )
 }
