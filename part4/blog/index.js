b7e0719c2f8e94972e247aa99df3f7174f9f3aba
captainkent <29131214+captainkent@users.noreply.github.com>
2022-07-13 18:18:58 +0200

    Refactored controllers to separate directory.

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
 
 const app = express();
 
@@ -13,19 +13,7 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
