13fbffe779e5576b492ff1bab2ccbf19019bbf2e
captainkent <29131214+captainkent@users.noreply.github.com>
2022-07-13 18:14:25 +0200

    Working blog with refactored models.

---
+++ part4/blog/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+const app = express();
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
