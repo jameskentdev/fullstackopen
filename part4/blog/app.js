4a5ae55a1d587c540b6f9f375b99398a9f0e8687
captainkent <29131214+captainkent@users.noreply.github.com>
2022-07-16 22:52:55 +0200

    Updated application to work with async routes. Added middleware for exception handling. Added verification to models.

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('express-async-errors');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config');
+const middleware = require('./utils/middleware');
 
 const app = express();
 
@@ -18,7 +20,11 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
