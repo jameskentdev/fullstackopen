9f066b0b41b515040eb23ef87b81dd67937200ea
captainkent <29131214+captainkent@users.noreply.github.com>
2022-07-16 22:53:23 +0200

    Updated eslint.

--- part4/blog/.eslintrc.js
+++ part4/blog/.eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 };
