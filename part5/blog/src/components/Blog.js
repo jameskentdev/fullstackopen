5c63c5583e6aab26e0d3278ee45b1b6691a6d730
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-11 22:21:10 +0100

    Blog component now shows more info and is togglable.

--- part5/blog/src/components/Blog.js
+++ part5/blog/src/components/Blog.js
@@ -1,7 +1,46 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const visibleOnClick = () => {
+    setVisible(!visible);
+  };
+  const likeOnClick = () => {};
+
+  return (
+    <div style={blogStyle}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div>
+          {blog.title} {blog.author}
+        </div>
+        {visible === true ? (
+          <>
+            <button onClick={visibleOnClick}>hide</button>
+          </>
+        ) : (
+          <button onClick={visibleOnClick}>view</button>
+        )}
+      </div>
+      {visible && (
+        <>
+          <div>{blog.url}</div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div>{blog.likes} likes</div>
+            <button onClick={likeOnClick}>like</button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
