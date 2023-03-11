8d378a9616c9bf14abe61bb9d9ee4b7d7b4687ef
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-11 15:18:11 +0100

    Added Notification component.

---
+++ part5/blog/src/components/Notification.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Notification = ({ error, message }) => {
+  if (message === '') {
+    return null;
+  }
+
+  const style = {
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px',
+  };
+
+  if (error) {
+    const error = { ...style, color: 'red' };
+    return <div style={error}>{message}</div>;
+  } else {
+    const ok = { ...style, color: 'green' };
+    return <div style={ok}>{message}</div>;
+  }
+};
+
+export default Notification;
