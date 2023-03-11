585a69f68f9d547c5fcb9ad948923299cab56d27
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-11 23:06:30 +0100

    Added ability to like a blog. Note: doesn't support re-render of the likes.

--- part5/blog/src/components/Blog.js
+++ part5/blog/src/components/Blog.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import _ from 'lodash';
+import blogService from '../services/blogs';
 
 const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   const blogStyle = {
     paddingTop: 10,
@@ -14,10 +17,22 @@ const Blog = ({ blog }) => {
   const visibleOnClick = () => {
     setVisible(!visible);
   };
-  const likeOnClick = () => {};
+
+  const likeOnClick = async () => {
+    // Increment the likes, strip the populated user and remove blog id
+    const updatedBlog = _.omit(
+      { ...blog, likes: blog.likes + 1, user: blog.user.id },
+      'id'
+    );
+    await blogService.update(blog.id, updatedBlog);
+
+    // Need to do this to force re-render
+    setToggle(!toggle);
+  };
 
   return (
     <div style={blogStyle}>
+      <div>{blog.id}</div>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <div>
           {blog.title} {blog.author}
