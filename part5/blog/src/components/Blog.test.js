dab8f5645eb641dbe1e5d6d41ff55d4e81bb3380
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-17 17:11:28 +0100

    Added Blog test. Updated test stuff to be in devDependencies. Added jest config to package.json.

---
+++ part5/blog/src/components/Blog.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+import Blog from '../components/Blog';
+
+test('Blog details shown when view is clicked', async () => {
+  expect(true).toBeTruthy();
+});
