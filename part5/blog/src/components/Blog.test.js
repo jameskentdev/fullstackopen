7130d869fd91f7955c98fe074ddd4819a6a5a90c
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-17 17:44:53 +0100

    Added blog tests.

--- part5/blog/src/components/Blog.test.js
+++ part5/blog/src/components/Blog.test.js
@@ -4,6 +4,58 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import Blog from '../components/Blog';
 
-test('Blog details shown when view is clicked', async () => {
-  expect(true).toBeTruthy();
+describe('<Blog>', () => {
+  let userSetup = null;
+  let likeHandler = null;
+
+  beforeEach(() => {
+    const blog = {
+      title: 'How to annoy Teddy',
+      author: 'Mishka Aldanova',
+      url: 'http://example.com',
+      likes: 1,
+      user: { username: 'mishka' },
+    };
+
+    const user = {
+      username: 'mishka',
+    };
+
+    userSetup = userEvent.setup();
+    likeHandler = jest.fn();
+    render(<Blog likeHandler={likeHandler} user={user} blog={blog} />);
+  });
+
+  test('Blog only render title/author', async () => {
+    const definedElement = screen.getByTestId('title-author');
+    expect(definedElement).toBeDefined();
+
+    const url = screen.queryByText('http://example.com');
+    expect(url).toBeNull();
+
+    const likes = screen.queryByText('1 likes');
+    expect(likes).toBeNull();
+  });
+
+  test('Blog show url/likes after clicking view', async () => {
+    const button = screen.getByRole('button');
+    await userSetup.click(button);
+
+    const url = screen.queryByText('http://example.com');
+    expect(url).toBeDefined();
+
+    const likes = screen.queryByText('1 likes');
+    expect(likes).toBeDefined();
+  });
+
+  test('Blog likeHandler called twice for two clicks', async () => {
+    const button = screen.getByRole('button');
+    await userSetup.click(button);
+
+    const likeButton = screen.getByText('like');
+    await userSetup.click(likeButton);
+    await userSetup.click(likeButton);
+
+    expect(likeHandler).toHaveBeenCalledTimes(2);
+  });
 });
